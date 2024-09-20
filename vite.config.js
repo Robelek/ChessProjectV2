c02978f34c3c053139825b0ed8a86264adb640ee
Robeleczek <robeleczek@gmail.com>
2024-09-20 17:24:46 +0200

    Added deploy workflow based on https://github.com/ErickKS/vite-deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: "/chessProjectV2/",
+    base: "/ChessProjectV2/",
     server: {
       port: 8001,
     },
